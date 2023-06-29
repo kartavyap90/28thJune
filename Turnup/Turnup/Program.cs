bfb39a8c4238f76d3e447f1bb068039123fc0829
kartavyap90 <kartavyap90@gmail.com>
2023-06-29 19:09:53 +1200

    Turnup login test created

---
+++ Turnup/Turnup/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+        driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+        IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+        usernameTextbox.SendKeys("hari");
+        IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+        passwordTextbox.SendKeys("123123");
+        IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+        loginButton.Click();
+        IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+        if (hellohari.Text == "Hello hari!")
+        {
+            Console.WriteLine("User has logged Successfully");
+        }
+        else
+        {
+            Console.WriteLine("User has not logged in Successfully");
+        }
+
+    }
+}
